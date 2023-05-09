aff93d0a4d5ab63d508df71eb8428cc870eba674
jeanpierre <alejandrovelaarana@gmail.com>
2023-05-09 17:56:08 -0300

    bdd

--- backend/src/mongodb.js
+++ backend/src/mongodb.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 // 127.0.0.1
-mongoose.connect('mongodb+srv://jean:20011015kiraynana@proyecto.nn5v7j5.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://jean:20011015kiraynana@proyecto.nn5v7j5.mongodb.net/the-clean-plate?retryWrites=true&w=majority', {
     
     useUnifiedTopology: true,
     useNewUrlParser: true,
