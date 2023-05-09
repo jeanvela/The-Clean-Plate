519bc0dce97214d3e9f805bb17c12f746efd207e
DiegoMirabelli <mirabelli_diego@outlook.com>
2023-05-09 02:52:58 -0300

    footer simple & carpeta styles

--- frontend/src/components/views/Home.jsx
+++ frontend/src/components/views/Home.jsx
@@ -1,6 +1,18 @@
-function Home() {
-  return <div>Home</div>;
-}
-//asdsa
-//comentario de prueba
+// import NavBar from "../NavBar/NavBar";
+import Footer from "./Footer";
+
+const Home = () => {
+  return (
+    <div>
+      {/* <NavBar/> */}
+
+      <div>
+        <h1 className="text-center">Titulo</h1>
+      </div>
+
+      <Footer />
+    </div>
+  );
+};
+
 export default Home;
