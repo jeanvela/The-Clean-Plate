0d69b13990e1c1f57cda8a38bfa90ea5f13c877b
gustimartin <gustavoemartin1991@gmail.com>
2023-05-09 17:27:55 -0300

    prod y cat

---
+++ frontend/src/components/categories/Category.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+function Category({ name, image, id }) {
+  return (
+    <div className="  rounded-lg relative  ">
+      <Link to="/categories/products">
+        <img
+          src={image}
+          alt="image"
+          className="object-cover h-40 w-full rounded-lg absolute mix-blend-overlay  opacity-75"
+        />
+        <h1 className=" text-4xl p-16  text-center font-bold text-black ">
+          {" "}
+          {name}{" "}
+        </h1>
+      </Link>
+    </div>
+  );
+}
+
+export default Category;
