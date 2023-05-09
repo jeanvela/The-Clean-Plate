f05e011e09386aa00a9af56f399fa5f8ff71e85f
elrobertrocha91 <robertoestebanrocha91@gmail.com>
2023-05-08 21:38:20 -0300

    Function Controllers

--- backend/src/controllers/productsControllers.js
+++ backend/src/controllers/productsControllers.js
@@ -1,21 +1,51 @@
 
 
-const createProduct = () => {
+const createProduct =  async (id, name, amount, category, image, price, description, stock, categoryId) => {
+    
+    const newProduct = new Product({
+        id,
+        name,
+        amount, 
+        category,
+        image, 
+        price,
+        description,
+        stock,
+        categoryId
+    });
+
+    return await newProduct.save();
 
 }
 
 
-const getProductById = () => {
+const getProductById = async (idProduct) => {
+
+    const productById = await Product.findById(idProduct);
+
+    return productById;
 
 }
 
 
-const getAllProducts = () => {
+const getAllProducts = async () => {
+    
+    const allProducts = await Product.find();
+
+    return allProducts;
 
 }
 
 
-const getProductByName = () => {
+const getProductByName = async (name) => {
+
+    const productByName = await Product.find({
+        name: {
+            $regex: name
+        }
+    });
+
+    return productByName;
 
 }
 
