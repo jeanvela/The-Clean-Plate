332ca42b74002c8652d62b4241bf9b667fa009d5
DiegoMirabelli <mirabelli_diego@outlook.com>
2023-05-10 10:16:41 -0300

    form aun sin terminar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Home from "../src/components/views/Home";
+import FormProdutcs from "./components/formProducts/FormProducts";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <div>
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route exact path="/createproduct" element={<FormProdutcs />} />
         </Routes>
       </div>
     </>
