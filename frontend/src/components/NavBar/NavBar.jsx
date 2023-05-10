32683664ba309be5a0184398e8344928dcda20d8
TiagoFernandez01 <tiagofernandezz01@hotmail.com>
2023-05-10 17:48:15 -0300

    correcion de  linkeo de navBar

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -6,10 +6,14 @@ function NavBar() {
     return (
         <>
             <div className="flex justify-between items-center flex-row bg-yellow-900 py-6 text-yellow-400 ">
-                <h2 className="font-normal text-2xl">The Clean Plate</h2>
+                <Link to='/'>
+                    <h2 className="font-normal text-2xl">The Clean Plate</h2>
+                </Link>
 
                 <SearchBar />
+
                 <div className="container-links">
+
                     <Link to="/">
                         <a className="mr-4 text-decoration-none rounded-lg hover:bg-yellow-700 ">Home</a>
                     </Link>
@@ -18,12 +22,16 @@ function NavBar() {
                         <a className="mr-4 text-decoration-none rounded-lg hover:bg-yellow-700 ">About</a>
                     </Link>
 
-                    <Link to="/category">
+                    <Link to="/categories">
                         <a className="mr-4 text-decoration-none rounded-lg hover:bg-yellow-700 ">Products</a>
                     </Link>
 
                     <Link to="/contact">
                         <a className="mr-4 text-decoration-none rounded-lg hover:bg-yellow-700 ">Contact</a>
+                    </Link>
+
+                    <Link to="/createproduct">
+                        <a className="mr-4 text-decoration-none rounded-lg hover:bg-yellow-700 ">Form</a>
                     </Link>
 
                 </div>
