b0370f82c380be12e3d790b10422b66ac16a579f
sofiavictoriafranco <sofiavictoriafranco@hotmail.com>
2023-05-11 11:56:59 -0300

    agregue prop origin

--- backend/src/routes/productsRoutes.js
+++ backend/src/routes/productsRoutes.js
@@ -5,9 +5,9 @@ const router = Router();
 
 const validate = (req,res,next) => {
 
-    const {name, price, category, description, stock} = req.body;
+    const {name, price, category, description, stock, origin} = req.body;
 
-    if(!name || !price || !category || !description || !stock) return res.status(400).json({error: 'Missing data'})
+    if(!name || !price || !category || !description || !stock || !origin) return res.status(400).json({error: 'Missing data'})
 
      next();
 
