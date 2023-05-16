1b786749f8866cfbc37a41c3a20153ae8eb73f49
gustimartin <gustavoemartin1991@gmail.com>
2023-05-16 15:19:18 -0300

    cartStyles

--- frontend/src/components/NavBar/cart/Cart.jsx
+++ frontend/src/components/NavBar/cart/Cart.jsx
@@ -32,10 +32,10 @@ function Cart() {
   };
 
   return (
-    <div className=" px-2  py-4 grid items-center grid-cols-4 grid-cols gap-2">
-      <h2 className=" font-normal text-3xl text-center">Shooping Cart</h2>
+    <div className=" px-2  py-4 ">
+      <h2 className=" font-normal text-3xl text-center ">Shooping Cart</h2>
       {cartItem.length === 0 ? (
-        <div className=" text-xl mt-8 flex-col items-center ">
+        <div className=" text-xl mt-8 flex-col  flex items-center ">
           <p>your cart is empty</p>
           <div className=" ml-4 text-gray-500 flex items-center mt-4">
             <Link to="/categories/products">
@@ -46,55 +46,84 @@ function Cart() {
       ) : (
         <div>
           <div className=" mt-8 mb-4 grid items-center grid-cols-4 gap-2">
-            <h3 className=" text-sm font-normal uppercase">Product</h3>
+            <h3 className=" text-sm font-normal uppercase pl-4">Product</h3>
             <h3 className=" text-sm font-normal uppercase">Price</h3>
             <h3 className=" text-sm font-normal uppercase">Amount</h3>
             <h3 className=" text-sm font-normal uppercase">Total</h3>
           </div>
-          <div className=" grid items-center grid-cols-4 gap-2">
+          <div className="">
             {cartItem?.map((item) => (
-              <div key={item.id}>
-                <div className=" ">
-                  <img src={item.image} alt={item.name} />
+              <div
+                className=" grid grid-cols-4  gap-2 items-center border-t-2 p-4"
+                key={item.id}
+              >
+                <div className="flex">
+                  <img
+                    className=" w-24 max-w-full ml-4"
+                    src={item.image}
+                    alt={item.name}
+                  />
                   <div>
                     <h3 className=" text-sm font-normal uppercase">
                       {item.name}
                     </h3>
                     <p>{item.category}</p>
-                    <button onClick={() => handelRemove(item)}>remove</button>
+                    <button
+                      className=" mt-2 cursor-pointer text-gray-300"
+                      onClick={() => handelRemove(item)}
+                    >
+                      remove
+                    </button>
                   </div>
                 </div>
+
                 <div>${item.price}</div>
-                <div>
-                  <button onClick={() => handeldecrease(item)}>-</button>
-                  <div>{item.cartAmount}</div>
-                  <button onClick={() => handleIncrement(item)}>+</button>
+                <div className=" flex-start flex justify-center w-16  max-w-full border-s-2    border-x-2  rounded-md  ">
+                  <button
+                    className="p-3 cursor-pointer"
+                    onClick={() => handeldecrease(item)}
+                  >
+                    -
+                  </button>
+                  <div className=" pt-3">{item.cartAmount}</div>
+                  <button
+                    className=" p-3  cursor-pointer"
+                    onClick={() => handleIncrement(item)}
+                  >
+                    +
+                  </button>
                 </div>
-                <div>${item.price * item.cartAmount}</div>
+                <div className=" justify-self-start pr-4  font-bold">
+                  ${item.price * item.cartAmount}
+                </div>
               </div>
             ))}
           </div>
-          <div>
+          <div className=" flex-start flex justify-between pt-4 border-t-2 ">
             <button
               onClick={() => handelClearCart()}
-              className=" max-w-full h-9 rounded-md font-normal bg-yellow-900 text-white cursor-pointer"
+              className=" max-w-full w-32  h-10 rounded-md font-normal bg-yellow-900 text-white cursor-pointer"
             >
               Clear cart
             </button>
-            <div>
-              <div>
+            <div className=" w-64 max-w-full">
+              <div className=" flex justify-between  text-lg  ">
                 <span>Subtotal</span>
-                <span>{cart.total}</span>
+                <span className=" font-bold mr-4">
+                  {" "}
+                  ${cart.total.toFixed(2)}
+                </span>
               </div>
-            </div>
-            <p>Free shipping</p>
-            <button className=" max-w-full h-9 rounded-md font-normal bg-yellow-900 text-white cursor-pointer">
-              Check Out
-            </button>
-            <div className=" mt-4">
-              <Link to="/categories/products">
-                <span className=" ml-2 flex items-center"> Keep shopping</span>
-              </Link>
+
+              <p className="  text-sm font-extralight my-2">Free shipping</p>
+              <button className=" max-w-full h-9 rounded-md font-normal bg-yellow-900 text-white cursor-pointer">
+                Check Out
+              </button>
+              <div className=" mt-4">
+                <Link to="/categories/products">
+                  <span>Keep shopping</span>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
