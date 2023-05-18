c72c69afc50fcd40ac11eeab0595cfb7bf02b83c
TiagoFernandez01 <tiagofernandezz01@hotmail.com>
2023-05-18 17:23:13 -0300

    Infinite Scroll

--- frontend/src/components/products/Cards.jsx
+++ frontend/src/components/products/Cards.jsx
@@ -1,20 +1,21 @@
 import Card from "./Card";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
   getAllProducts,
   filterByCategory,
   filterByOrigin,
 } from "../../features/productsSlice";
-
 import { getAllCategories } from "../../features/categorySlice";
+import InfiniteScroll from "react-infinite-scroll-component";
+
 function Cards() {
   const dispatch = useDispatch();
-  const { allCategories: categories } = useSelector(
-    (state) => state.categories
-  );
-
+  const { allCategories: categories } = useSelector((state) => state.categories);
   const { products } = useSelector((state) => state.products);
+
+  const [itemsToShow, setItemsToShow] = useState(20);
+  const [hasMore, setHasMore] = useState(true)
 
   useEffect(() => {
     dispatch(getAllProducts());
@@ -24,39 +25,72 @@ function Cards() {
   const handlerFilterByCategory = (e) => {
     e.preventDefault();
     dispatch(filterByCategory(e.target.value));
+
   };
 
   const handlerFilterByOrigin = (e) => {
     e.preventDefault();
     dispatch(filterByOrigin(e.target.value));
+
   };
 
-  return (
-    <div className=" grid grid-cols-3 gap-1 mt-5 mx-2 grid-rows-3">
-      <select onChange={handlerFilterByCategory}>
-        <option disabled selected defaultValue>
-          Categories
-        </option>
-        <option value="All">All</option>
-        {categories?.map((el) => (
-          <option value={el.name} key={el.id}>
-            {el.name}
-          </option>
-        ))}
-      </select>
 
-      <select onChange={handlerFilterByOrigin}>
-        <option disabled selected defaultValue>
-          Origin
-        </option>
-        <option value="All">All</option>
-        <option value="animal">Animal</option>
-        <option value="plant">Plant</option>
-      </select>
-      {products?.length
-        ? products.map((card) => (
-            <ul key={card._id}>
+  const fetchMoreData = () => {
+    setTimeout(() => {
+      const additionalItems = products.slice(itemsToShow, itemsToShow + 20);
+      setItemsToShow(itemsToShow + additionalItems.length)
+      if (itemsToShow >= products.length){
+        setHasMore(false)
+      }
+    }, 1000);
+
+  };
+
+  const displayedProducts = products.slice(0, itemsToShow);
+
+
+  return (
+    <div className="mt-5 mx-2"> 
+      <div className="flex justify-between mb-5">
+        <select
+          className="border border-gray-300 rounded px-2 py-1"
+          onChange={handlerFilterByCategory}
+        >
+          <option disabled selected defaultValue>
+            Categories
+          </option>
+          <option value="All">All</option>
+          {categories?.map((el) => (
+            <option value={el.name} key={el.id}>
+              {el.name}
+            </option>
+          ))}
+        </select>
+
+        <select
+          className="border border-gray-300 rounded px-2 py-1"
+          onChange={handlerFilterByOrigin}
+        >
+          <option disabled selected defaultValue>
+            Origin
+          </option>
+          <option value="All">All</option>
+          <option value="animal">Animal</option>
+          <option value="plant">Plant</option>
+        </select>
+      </div>
+
+      <div  id="InfiniteScroll">
+        <InfiniteScroll
+          dataLength={displayedProducts.length}
+          next={fetchMoreData}
+          hasMore={hasMore}
+          loader={<h4>Loading...</h4>}
+        >
+          <div className="grid grid-cols-3 gap-1 mt-5 mx-2 grid-rows-3"> {displayedProducts?.length ? (
+            displayedProducts.map((card) => (
               <Card
+                key={card._id}
                 name={card.name}
                 image={card.image}
                 category={card.category[0]}
@@ -64,11 +98,17 @@ function Cards() {
                 price={card.price}
                 id={card._id}
               />
-            </ul>
-          ))
-        : console.log({ products })}
+            ))
+          ) : (
+            <p>No products found.</p>
+          )}</div>
+         
+        </InfiniteScroll>
+      </div>
     </div>
   );
 }
 
 export default Cards;
+
+
