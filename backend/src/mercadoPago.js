b231f1552c6062d9a8e5db2af071f861e88e27ac
gustimartin <gustavoemartin1991@gmail.com>
2023-05-23 10:53:03 -0300

    payment in database

--- backend/src/mercadoPago.js
+++
@@ -1,9 +0,0 @@
-// const mercadopago = require("mercadopago");
-// require("dotenv").config();
-// const { ACCESS_TOKEN } = process.env;
-
-// mercadopago.configure({
-//     access_token: ACCESS_TOKEN,
-// });
-
-// module.exports = { mercadopago };
