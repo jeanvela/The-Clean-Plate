2917c8f090b06a4cc737e73de11f41d2db440030
jeanpierre <alejandrovelaarana@gmail.com>
2023-05-23 12:19:45 -0300

    get-users

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,19 +1,10 @@
 const { Router } = require('express');
-
 const {authHandler} = require('../handlers/authHandler')
-
+const { getUser } = require('../handlers/authHandler')
 
 const router = Router();
 
 router.post('/', authHandler)
-
-
-
-
-
-
-
-
-
+router.get('/',getUser)
 
 module.exports = router;
